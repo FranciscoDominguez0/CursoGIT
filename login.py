60694ef02f97e996a864db1cb7d28853e54a0a2b
Francisco <dominguezf225@gmail.com>
2025-06-21 20:30:57 -0500

    login v2

--- login.py
+++ login.py
@@ -1,12 +1,3 @@
 # Un login sencillo en Python
 
-usuario_correcto = "admin"
-contrasena_correcta = "1234"
-
-usuario = input("Usuario: ")
-contrasena = input("Contraseña: ")
-
-if usuario == usuario_correcto and contrasena == contrasena_correcta:
-    print("¡Login exitoso!")
-else:
-    print("Usuario o contraseña incorrectos.")
+print("Bienvenido al sistema de login")
